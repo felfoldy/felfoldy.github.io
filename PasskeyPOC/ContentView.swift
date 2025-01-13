c94590811c51212cc6b8f5099f51c57a99a35268
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2025-01-13 11:41:53 +0100

    Initial Commit

---
+++ PasskeyPOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PasskeyPOC
+//
+//  Created by Tibor Felföldy on 2025-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PasskeyPOC/PasskeyPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasskeyPOCApp.swift
+//  PasskeyPOC
+//
+//  Created by Tibor Felföldy on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct PasskeyPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
