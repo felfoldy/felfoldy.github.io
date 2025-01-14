f3e3c9fd842bf75e5363457ac828e4372fd648d9
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2025-01-14 13:44:03 +0100

    Add create passkey code

--- PasskeyPOC/ContentView.swift
+++ PasskeyPOC/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct ContentView: View {
+    @Environment(\.authorizationController) private var authorizationController
+    
+    @State var userID: String = ""
+    @State var challenge: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            TextField("userID", text: $userID)
+            
+            TextField("challenge", text: $challenge)
+            
+            Button("create passkey") {
+                let platformProvider = ASAuthorizationPlatformPublicKeyCredentialProvider(relyingPartyIdentifier: "felfoldy.github.io")
+                
+                let challengeData = challenge.data(using: .utf8)!
+                let userIDData = userID.data(using: .utf8)!
+                
+                let request = platformProvider.createCredentialRegistrationRequest(
+                    challenge: challengeData,
+                    name: "Some Name",
+                    userID: userIDData
+                )
+                
+                performRequest(request: request)
+            }
         }
         .padding()
+    }
+    
+    func performRequest(request: ASAuthorizationRequest) {
+        Task {
+            do {
+                let result = try await authorizationController.performRequest(request)
+                
+                print(result)
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
